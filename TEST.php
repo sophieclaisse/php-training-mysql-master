89f10f257e27c547a3ff7b038532e108a6c471cf
sophieclaisse <43751485+sophieclaisse@users.noreply.github.com>
2019-01-16 14:57:14 +0100

    Revert "CRUD 2"
    This reverts commit c18458a9acb3b721fef85ca150f5af04aefc9638.

--- TEST.php
+++ TEST.php
@@ -6,68 +6,12 @@
  * Time: 15:57
  */
 
+//FORMULE TEST POUR LE UPDATE - ERROR (FONCTIONNEL SUR READ.PHP)
 
-//FILTRES DE VALIDATION
-
-if (isset($_GET['id'])){
-    $id= $_GET['id'];}
+//echo " - " . $row['id']." - " ?> <a href="<?php echo'update.php?name='. $row['name']?> <a/> <?php echo " -  " . $row['difficulty'] ." -  " . $row['distance'] ." -  " . $row['duration'] ." -  " . $row['height_difference'] ."<br><br>";
 
 
-$sql= "SELECT hiking.* FROM hiking where id='$id'";
-$sel = $conn->query($sql);
-
-$selection=$sel->fetch_assoc();
-
-
-function changerData ($id)
-{
-
-    if (isset($_POST['name'])){
-        $name= $_POST['name'];}
-
-    if (isset($_POST['distance'])){
-        $distance= $_POST['distance'];}
-
-    if (isset($_POST['difficulty']) ){
-        $difficulty= $_POST['difficulty'];}
-
-    if (isset($_POST['duration']) ){
-        $duration= $_POST['duration'];}
-
-    if (isset($_POST['height_difference'])){
-        $denivele=$_POST['height_difference'];}
-
-    global $conn;
-
-
-
-    $aJour= "UPDATE hiking set  
-              `name`='$name',
-              difficulty='$difficulty',
-              distance='$distance', 
-              duration='$duration', 
-              height_difference='$denivele' 
-               
-              where id = $id";
-
-
-    $conn->query($aJour);
-    /* echo $aJour;
-     echo $conn->error;*/
-}
-
-if ((isset ($_POST['name']) && (!filter_var($_POST['distance'], FILTER_VALIDATE_INT) === false)) && (!filter_var($_POST['distance'], FILTER_VALIDATE_INT) === false) && (!filter_var($_POST['distance'], FILTER_VALIDATE_INT) === false)){
-
-    changerData($id);
-
-}
-
-/*
-if (!filter_var($int, FILTER_VALIDATE_INT) === false) {
-    echo("Integer is valid");
-} else {
-    echo("Integer is not valid");
-}
-
-*/
-
+//BONNE FORMULE JUSTE POUR LE DELETE
+//
+//
+// <?php echo " - " . $row['id']." - " . $row['name'] . " -  " . $row['difficulty'] ." -  " . $row['distance'] ." -  " . $row['duration'] ." -  " . $row['height_difference'] ."<br><br>";
